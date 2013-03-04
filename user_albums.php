30ae4957c3dc5c3c4100af16dfb723877b07a372
champuli <champuli86@gmail.com>
2013-03-04 14:42:25 +0200

    dobavlenie rand pic na glavnoy albomov polzovateley

--- user_albums.php
+++ user_albums.php
@@ -18,27 +18,43 @@ while($s = mysql_fetch_assoc($show_alb))
 {
     $all_show[] = $s;
 }
+
+$r = array();
+$rand = array();
+$rand_pic = mysql_query("SELECT * FROM `pics` order by rand() limit 1");
+
+while($r = mysql_fetch_assoc($rand_pic))
+{
+    $rand[] = $r;
+}
+
+// echo "<pre>";
+// print_r($rand);
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="style.css">
-<title>album admin area</title>
+<title>Album User Area</title>
 </head>
 <body>
 <div id="main">
-   <div id="content"> 
-   <form enctype="multipart/form-data" action="/gallery/user_albums.php" method="get" > 
+    <div id="content"> 
+       <?php foreach ($rand as $r_k => $r_v): ?>
+            <img  width="400px" src="http://localhost/gallery/files/<?php echo $r_v['path_big']; ?>" />
+            <?php endforeach; ?>
 
-        <?php foreach ($all_show as $nu => $val): ?>
-        <div class="album_cover">
-            <a href="http://localhost/gallery/foto_in_user_albums?album=<?php echo $val['id']; ?>"><?php echo $val['name']; ?></a><br />
-            <img src="http://localhost/gallery/files/<?php echo $val['path_oblogka']; ?>" />
-        </div>
-        <?php endforeach; ?>
-        <br /><br />
-
+       <form enctype="multipart/form-data" action="/gallery/user_albums.php" method="get" > 
+            
+            <?php foreach ($all_show as $nu => $val): ?>
+            <div class="album_cover">
+                <a href="http://localhost/gallery/foto_in_user_albums?album=<?php echo $val['id']; ?>"><?php echo $val['name']; ?></a><br />
+                <img src="http://localhost/gallery/files/<?php echo $val['path_oblogka']; ?>" />
+            </div>
+            <?php endforeach; ?>
+            <br /><br />
     </div> 
 
 </div>
